da2ecb72dabbf38cd1375c06bafecdcc31cbd43e
sruthi kommati <sruthi.k22@iiits.in>
2026-02-01 17:18:37 +0530

    Solved cors issue

--- src/server.js
+++ src/server.js
@@ -16,7 +16,14 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
